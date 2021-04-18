6d4e39cd0f377f3a840e4133c143588a3038dedc
kevin <kevin@diamond-sky.com>
2021-04-18 20:18:54 +0100

    Merge the get request with the post request

--- app/javascript/posts/api.js
+++ app/javascript/posts/api.js
@@ -1,28 +1,10 @@
-// posts/api.js
 import {server} from 'remote/server'
-import * as axios from 'axios'
-
 
 export function fetch(id) {
   return server.send(`/posts/${id}.json`)
 }
 
 export function create(post) {
-  const request = {
-    url: '/posts.json',
-    method: 'post',
-    data: {post},
-    headers: {
-      'X-CSRF-Token': getCSRFToken(),
-    }
-  }
-
-  return axios(request)
-      .then(response => console.log(response.data))
-      .catch(error => console.log(error))
+  return post('/posts.json', 'post', {post})
 }
 
-export function getCSRFToken() {
-  const csrfTag = document.querySelector('meta[name=csrf-token]') || {content: 'missing-csrf-token'}
-  return csrfTag.content
-}
