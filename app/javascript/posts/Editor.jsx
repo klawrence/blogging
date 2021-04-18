4d171480321ad338d29a9c1ce2b3f23341f3fa84
kevin <kevin@diamond-sky.com>
2021-04-18 19:46:41 +0100

    Extract the editor

--- app/javascript/posts/Editor.jsx
+++ app/javascript/posts/Editor.jsx
@@ -1,11 +1,42 @@
 import React from 'react'
 
-export function Editor() {
-  return <form>
-    <input type='text' name='title'  className='title'/>
-    <textarea name='body' className='body' />
-    <div className='actions'>
-      <input type='submit' className='button'/>
-    </div>
-  </form>
+export default class Editor extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      post: {}
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  render() {
+    const {post} = this.state
+
+    return <form onChange={this.handleChange}
+                 onSubmit={this.handleSubmit}>
+      <input type='text' name='title' className='title' defaultValue={post.title} />
+      <textarea name='body' className='body' defaultValue={post.body} />
+      <div className='actions'>
+        <input type='submit' className='button' />
+      </div>
+    </form>
+  }
+
+  handleChange(event) {
+    const {name, value} = event.target
+    const post = {
+      ...this.state.post,
+      [name]: value
+    }
+    this.setState({post})
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+
+    const {post} = this.state
+    const {onSubmit} = this.props
+    onSubmit && onSubmit(post)
+  }
 }
