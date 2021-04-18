1a89a750069f6820068cd955c79dc0af534a72e8
kevin <kevin@diamond-sky.com>
2021-04-18 14:59:04 +0100

    Add posts#show

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,8 +1,23 @@
 require "test_helper"
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @post = Post.create! title: 'The title', body: 'The body.'
+  end
+
   test 'should get index' do
     get posts_url
     assert_response :success
   end
+
+  test 'fetch a post as json' do
+    get post_url(@post, format: :json)
+    assert_response :success
+
+    json = JSON.parse response.body, symbolize_names: true
+    assert_equal @post.id, json[:id]
+    assert_equal 'The title', json[:title]
+    assert_equal 'The body.', json[:body]
+  end
+
 end
