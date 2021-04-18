9e0a5aebb2258a70ffc475ffe77eb65e023489ee
kevin <kevin@diamond-sky.com>
2021-04-18 17:21:43 +0100

    Extract a post component

---
+++ test/javascript/posts/Post.test.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {mount} from 'enzyme'
+import {configure} from 'enzyme'
+// import Adapter from 'enzyme-adapter-react-17'
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+configure({ adapter: new Adapter() })
+
+import {Post} from 'posts/Post'
+
+describe('The post component', () => {
+  test('shows a blog post', () => {
+    const component = mount(<Post />)
+    expect(component.find('.title').text()).toBe('React on Rails')
+    expect(component.find('.body').text()).toBe('I can use React with Rails.')
+  })
+})
