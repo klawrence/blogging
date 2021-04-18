8baf0e8f876a286a44bbac2c202b0a0e65acc258
kevin <kevin@diamond-sky.com>
2021-04-18 20:30:46 +0100

    Show a list of posts

--- app/javascript/application/Main.jsx
+++ app/javascript/application/Main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Editor from '../posts/Editor'
-import Post from '../posts/Post'
 import {create} from '../posts/api'
+import List from '../posts/List'
 
 export function Main() {
   return <div id='main'>
     <Editor onSubmit={create}/>
-    <Post id={1} />
+    <List />
   </div>
 }
---
+++ app/javascript/posts/List.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import {fetch} from './api'
+
+export default class List extends React.Component {
+  render() {
+    return 'list of posts'
+  }
+}
