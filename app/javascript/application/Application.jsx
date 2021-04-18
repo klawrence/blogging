49341d8f0af67c09c305cbe0993d8d9707e65296
kevin <kevin@diamond-sky.com>
2021-04-18 19:09:49 +0100

    Add a post editor

--- app/javascript/application/Application.jsx
+++ app/javascript/application/Application.jsx
@@ -5,6 +5,11 @@ import Post from 'posts/Post'
 export function Application(_props) {
   return <div id='application'>
     <h1 className='site-name'>Blogging</h1>
-    <Post />
+    <form>
+      <input type='text' name='title' />
+      <textarea name='body' />
+      <input type='submit' />
+    </form>
+    <Post id={1} />
   </div>
 }
