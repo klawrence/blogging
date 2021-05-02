8901ce1d19e2430a94cd5a2ebb58edebc5638308
kevin <kevin@diamond-sky.com>
2021-05-02 12:34:30 +0100

    fetch a list of posts

--- app/javascript/posts/List.jsx
+++ app/javascript/posts/List.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
-import {fetch} from './api'
+import {fetch, list} from './api'
+import {Post} from './Post'
 
 export default class List extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      posts: null
+    }
+  }
+
   render() {
-    return 'list of posts'
+    const {posts} = this.state
+    if(posts === null) return 'Loading…'
+
+    return <ul>
+      {
+        posts.map(post => <li key={post.id}>
+          <Post post={post}/>
+        </li>)
+      }
+    </ul>
+  }
+
+  async componentDidMount() {
+    const posts = list()
+    this.setState({posts})
   }
 }
--- app/javascript/posts/api.js
+++ app/javascript/posts/api.js
@@ -1,10 +1,14 @@
 import {server} from 'remote/server'
 
 export function fetch(id) {
-  return server.send(`/posts/${id}.json`)
+  return server.get(`/posts/${id}.json`)
+}
+
+export function list() {
+  return server.get(`/posts.json`)
 }
 
 export function create(post) {
-  return post('/posts.json', 'post', {post})
+  return server.post('/posts.json', 'post', {post})
 }
 
