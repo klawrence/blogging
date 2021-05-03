c7aee0161d0480e9af06ee39408e4b848d1d067c
kevin <kevin@diamond-sky.com>
2021-05-03 12:57:45 +0100

    Add a sign in form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,25 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    credentials = credential_params
+
+    login(credentials[:email], credentials[:password]) do |user, failure|
+      if failure
+        @email = credentials[:email]
+        @error = failure
+        render action: 'new'
+      else
+        redirect_back_or_to(:root)
+      end
+    end
+  end
+
+  private
+  def credential_params
+    params.
+        require(:credentials).
+        permit(:email, :password)
+  end
 end
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @sally = User.create! email: 'sally@example.com',
+                          password: 'letmein'
+  end
+
   test 'sign in renders a form' do
     get sign_in_url
     assert_response :success
@@ -9,6 +14,33 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
       assert_select '#credentials_email'
       assert_select '#credentials_password'
       assert_select 'input.submit'
+    end
+  end
+
+  test 'redirect to home page after sign in' do
+    post sessions_url, params: {
+        credentials: {
+            email: 'sally@example.com',
+            password: 'letmein',
+        }
+    }
+    assert_redirected_to root_url
+  end
+
+  test 'rerender the form if sign in fails' do
+    post sessions_url, params: {
+        credentials: {
+            email: 'sally@wrong.com',
+            password: 'wrong',
+        }
+    }
+    assert_response :success
+
+    assert_select '.form' do
+      assert_select '.error', 'Invalid Login'
+      assert_select '#credentials_email' do |elements|
+        assert_equal 'sally@wrong.com', elements[0][:value]
+      end
     end
   end
 end
